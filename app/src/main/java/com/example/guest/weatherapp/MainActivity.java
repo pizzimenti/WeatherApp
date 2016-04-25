9a1a704147d0667f62192d811b96cb6818a059a0
Brad Pizzimenti and Mike Gallegos <mike@>
2016-04-25 15:01:34 -0700

    add binding for transition to 7-day view

--- app/src/main/java/com/example/guest/weatherapp/MainActivity.java
+++ app/src/main/java/com/example/guest/weatherapp/MainActivity.java
@@ -1,13 +1,16 @@
 package com.example.guest.weatherapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 import butterknife.Bind;
+import butterknife.ButterKnife;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private static final String TAG = MainActivity.class.getSimpleName();
     @Bind(R.id.findWeatherButton) Button mFindWeatherButton;
     @Bind(R.id.locationEditText) EditText mLocationEditText;
@@ -16,7 +19,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
 
+        mFindWeatherButton.setOnClickListener(this);
+    }
 
+    @Override
+    public void onClick(View v) {
+        if(v == mFindWeatherButton) {
+            String location = mLocationEditText.getText().toString();
+            Intent intent = new Intent (MainActivity.this, SevenDayActivity.class);
+            intent.putExtra("location", location);
+            startActivity(intent);
+        }
     }
 }

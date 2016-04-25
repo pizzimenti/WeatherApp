487e7060f767ea68100b7b4c9cc1247af6fc1a80
Brad Pizzimenti <brad@gennakersystems.com>
2016-04-25 15:25:56 -0700

    Log out submitted zip code

--- app/src/main/java/com/example/guest/weatherapp/SevenDayActivity.java
+++ app/src/main/java/com/example/guest/weatherapp/SevenDayActivity.java
@@ -1,13 +1,21 @@
 package com.example.guest.weatherapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class SevenDayActivity extends AppCompatActivity {
+    public static final String TAG = SevenDayActivity.class.getSimpleName();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_seven_day);
+
+        Intent intent = getIntent();
+        String location = intent.getStringExtra("location");
+
     }
 }
